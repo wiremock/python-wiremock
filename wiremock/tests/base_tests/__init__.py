051f9278a285380217afcba0c40a86bc3678f60a
John Harrison <johnpaulharrison@gmail.com>
2017-04-24 20:46:41 +0100

    Handle 201 status code from WireMock (#1)
    * Accept 201 status when posting new mapping
    
    Wiremock standalone (v2.6.0) returns `201 Created` status when
    creating new mappings via a POST request.
    
    `RestClient.handle_response()` updated accordingly with supporting
    test.
    
    * Remove IDE auto-templating
    
    - docstring TODOs
    - copyright statements

---
+++ wiremock/tests/base_tests/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+"""Tests for base sub-package."""
---
+++ wiremock/tests/base_tests/base_resource_tests.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""Tests for base_resource module."""
+
+import unittest
+
+from requests import Response
+
+from wiremock.base import RestClient
+from wiremock.exceptions import UnexpectedResponseException
+from wiremock.tests import BaseClientTestCase
+
+
+class RestClientTestCase(BaseClientTestCase):
+
+    def setUp(self):
+        super(RestClientTestCase, self).setUp()
+        self.client = RestClient()
+
+    def test_handle_response(self):
+        for status_code in [200, 201, 204]:
+            resp = self._create_dummy_response(status_code)
+            returned = self.client.handle_response(resp)
+            self.assertEqual(returned, resp)
+
+        resp = self._create_dummy_response(203)
+        with self.assertRaises(UnexpectedResponseException):
+            self.client.handle_response(resp)
+
+    # Helpers
+
+    def _create_dummy_response(self, status_code=200):
+        resp = Response()
+        resp.status_code = status_code
+        return resp
+
+
+if __name__ == '__main__':
+    unittest.main()
